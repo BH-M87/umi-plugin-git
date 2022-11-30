8811411c3791a9a0d70efb1de806bfb9c1749c10
Xuheng Liu <liuxuheng1990@gmail.com>
2022-11-30 22:15:00 +0800

    feat: add the git branch and commit hash meta info into index.html

---
+++ README.md
@@ -0,0 +1,38 @@
+# umi-plugin-git
+
+[![NPM version](https://img.shields.io/npm/v/umi-plugin-git.svg?style=flat)](https://npmjs.org/package/umi-plugin-git)
+[![NPM downloads](http://img.shields.io/npm/dm/umi-plugin-git.svg?style=flat)](https://npmjs.org/package/umi-plugin-git)
+
+A umi plugin to log the git info into index.html during every build
+
+## Install
+
+```bash
+# or yarn
+$ npm install
+```
+
+```bash
+$ npm run build --watch
+$ npm run start
+```
+
+## Usage
+
+Configure in `.umirc.js`,
+
+```js
+export default {
+  plugins: [
+    ['umi-plugin-git'],
+  ],
+}
+```
+
+## Options
+
+TODO
+
+## LICENSE
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { IApi } from 'umi';
+import { execSync } from 'node:child_process';
+
+export default async function(api: IApi) {
+  api.logger.info('use git plugin');
+  const gitLogBuffer = execSync('git log');
+  const gitLog = gitLogBuffer.toString();
+  const latestCommitLine = gitLog.split('\n')[0];
+  const latestCommitHash = latestCommitLine.split(' ')[1];
+  api.logger.info('Latest commit hash: ', latestCommitHash);
+  const gitStatusBuffer = execSync('git status');
+  const gitStatus = gitStatusBuffer.toString();
+  const onBranchLine = gitStatus.split('\n')[0];
+  const onBranch = onBranchLine.split(' ')[2];
+  api.logger.info('On branch: ', onBranch);
+  api.modifyHTML($ => {
+    $('head').prepend(
+      `<meta name="Current build commit hash" content="${latestCommitHash}"></meta>`,
+    );
+    $('head').prepend(
+      `<meta name="Current build branch" content="${onBranch}"></meta>`,
+    );
+    return $;
+  });
+}
