2d05c5da121689643cce73485d7571a94e97e7ae
Xuheng Liu <liuxuheng1990@gmail.com>
2022-12-02 14:17:50 +0800

    feat: add try catch

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,29 @@ import { IApi } from 'umi';
 import { execSync } from 'node:child_process';
 
 export default async function(api: IApi) {
-  api.logger.info('use git plugin');
-  const gitLogBuffer = execSync('git log', { maxBuffer: 100 * 1024 * 1024 }); // 100MB, use default 200KB will throw error, 10000 commit's size is around 4MB
-  const gitLog = gitLogBuffer.toString();
-  const latestCommitLine = gitLog.split('\n')[0];
-  const latestCommitHash = latestCommitLine.split(' ')[1];
-  api.logger.info('Latest commit hash: ', latestCommitHash);
-  const gitStatusBuffer = execSync('git status');
-  const gitStatus = gitStatusBuffer.toString();
-  const onBranchLine = gitStatus.split('\n')[0];
-  const onBranch = onBranchLine.split(' ')[2];
-  api.logger.info('On branch: ', onBranch);
-  api.modifyHTML($ => {
-    $('head').prepend(
-      `<meta name="Current build commit hash" content="${latestCommitHash}"></meta>`,
-    );
-    $('head').prepend(
-      `<meta name="Current build branch" content="${onBranch}"></meta>`,
-    );
-    return $;
-  });
+  api.logger.info('Use git plugin');
+  try {
+    const gitLogBuffer = execSync('git log', { maxBuffer: 100 * 1024 * 1024 }); // 100MB, use default 200KB will throw error, 10000 commit's size is around 4MB
+    const gitLog = gitLogBuffer.toString();
+    const latestCommitLine = gitLog.split('\n')[0];
+    const latestCommitHash = latestCommitLine.split(' ')[1];
+    api.logger.info('Latest commit hash: ', latestCommitHash);
+    const gitStatusBuffer = execSync('git status');
+    const gitStatus = gitStatusBuffer.toString();
+    const onBranchLine = gitStatus.split('\n')[0];
+    const onBranch = onBranchLine.split(' ')[2];
+    api.logger.info('On branch: ', onBranch);
+    api.modifyHTML($ => {
+      $('head').prepend(
+        `<meta name="Current build commit hash" content="${latestCommitHash}"></meta>`,
+      );
+      $('head').prepend(
+        `<meta name="Current build branch" content="${onBranch}"></meta>`,
+      );
+      return $;
+    });
+    api.logger.info('Use git plugin to modify html success.');
+  } catch (error) {
+    api.logger.error('Use git plugin failed: ', error);
+  }
 }
