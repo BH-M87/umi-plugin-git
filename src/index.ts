ac6eb89c7c4800dd2c3b847623e1fa6e9a5325b3
Xuheng Liu <liuxuheng1990@gmail.com>
2022-12-05 21:02:11 +0800

    feat: add build time info

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,30 @@ import { execSync } from 'node:child_process';
 export default async function(api: IApi) {
   api.logger.info('Use git plugin');
   try {
-    const gitLogBuffer = execSync('git log', { maxBuffer: 100 * 1024 * 1024 }); // 100MB, use default 200KB will throw error, 10000 commit's size is around 4MB
-    const gitLog = gitLogBuffer.toString();
-    const latestCommitLine = gitLog.split('\n')[0];
-    const latestCommitHash = latestCommitLine.split(' ')[1];
-    api.logger.info('Latest commit hash: ', latestCommitHash);
+    const now = Date();
+    api.logger.info('Current build time: ', now);
+
     const gitStatusBuffer = execSync('git status');
     const gitStatus = gitStatusBuffer.toString();
     const onBranchLine = gitStatus.split('\n')[0];
     const onBranch = onBranchLine.split(' ')[2];
     api.logger.info('On branch: ', onBranch);
+
+    const gitLogBuffer = execSync('git log', { maxBuffer: 100 * 1024 * 1024 }); // 100MB, use default 200KB will throw error, 10000 commit's size is around 4MB
+    const gitLog = gitLogBuffer.toString();
+    const latestCommitLine = gitLog.split('\n')[0];
+    const latestCommitHash = latestCommitLine.split(' ')[1];
+    api.logger.info('Latest commit hash: ', latestCommitHash);
+
     api.modifyHTML($ => {
       $('head').prepend(
         `<meta name="Current build commit hash" content="${latestCommitHash}"></meta>`,
       );
       $('head').prepend(
         `<meta name="Current build branch" content="${onBranch}"></meta>`,
+      );
+      $('head').prepend(
+        `<meta name="Current build time" content="${now}"></meta>`,
       );
       return $;
     });
